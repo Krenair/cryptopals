7cb1c4a65f1d62b6c0a2b31d8157c06e5e0eb802
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2021-04-17 21:34:13 +0100

    Challenge 3 complete

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -1,21 +1,34 @@
 import collections # TODO: cheating?
 from challenge1 import hexdecode
-s = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-decoded = list(hexdecode(s))
-print(decoded)
-(most_common_char, _), *_ = collections.Counter(decoded).most_common() # get most common character
-print(collections.Counter(decoded).most_common())
-# most common character in English is E, so assume E maps to our most_common_char
-# 'E' ^ key = most_common_char
-# TODO: is this right? most_common_char ^ 'E' = key
-print(most_common_char)
-#alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-print(ord('E')) # TODO: cheating?
-key = most_common_char ^ ord('E')
-print('Chosen key', key)
+english_freqs = {
+    # Based on https://en.wikipedia.org/wiki/Letter_frequency
+    'a': 8.2,   'b': 1.5,   'c': 2.8,  'd': 4.3, 'e': 13,  'f': 2.2,  'g': 2,   'h': 6.1,
+    'i': 7,     'j': 0.15,  'k': 0.77, 'l': 4,   'm': 2.4, 'n': 6.7,  'o': 7.5, 'p': 1.9,
+    'q': 0.095, 'r': 6,     's': 6.3,  't': 9.1, 'u': 2.8, 'v': 0.98, 'w': 2.4, 'x': 0.15,
+    'y': 2,     'z': 0.074,
+    ' ': 14 # the page says the space is more common than E, so add 1 to E's
+}
 
-o = []
-for c in decoded:
-	o.append(c ^ key)
+def single_byte_xor(text, k):
+    o = ''
+    for b in text:
+        o += chr(b ^ k) # TODO: cheating?
+    return o
 
-print(''.join(list(map(lambda x: chr(x), o))))
+if __name__ == "__main__":
+    s = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    print('Input:', s)
+    decoded = list(hexdecode(s))
+
+    scores = collections.Counter()
+    for key in range(256): # brute force
+        score = 0
+        for ch in single_byte_xor(decoded, key):
+            lch = ch.lower() # TODO: cheating?
+            score += english_freqs.get(lch, 0)
+        scores[key] = score
+
+    (k, _), *_ = scores.most_common()
+
+    print('Key:', k)
+    print('Output:', single_byte_xor(decoded, k))
