bf58cd76a2c402ce1acfb822f90bdc516c10d47f
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2021-05-28 12:21:52 +0100

    solved challenge12 - my plaintext was too short

--- set2/challenge11.py
+++ set2/challenge11.py
@@ -29,19 +29,27 @@ def encryption_oracle(input):
 
     if random.randint(0, 1) == 1: # assuming this is equivalent to rand(2)
         print('Mode: ECB')
-        return encrypt_aes_ecb(pad(input, 16), key)
+        return 'ECB', encrypt_aes_ecb(pad(input, 16), key)
     else:
         print('Mode: CBC')
-        return encrypt_aes_cbc(input, key, os.urandom(16))
+        return 'CBC', encrypt_aes_cbc(input, key, os.urandom(16))
 
 if __name__ == "__main__":
-    ct = list(encryption_oracle(b"ABCDABCDABCDABCD"))
-    print(ct)
-    # TODO: Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
+    for _ in range(100):
+        real_mode, ct_gen = encryption_oracle(b"ABCD" * 20)
+        ct = list(ct_gen)
+        print('Ciphertext', ct)
 
-    chunks = []
-    for i in range(0, len(ct), 16):
-        chunks.append(tuple(ct[i+j] for j in range(16) if i+j < len(ct)))
-    unique_chunks = len(set(chunks))
-    repetitions = len(chunks) - unique_chunks # going to look for line with most repeating blocks of 16 bytes
-    print(len(chunks), unique_chunks, repetitions)
+        chunks = []
+        for i in range(0, len(ct), 16):
+            chunks.append(tuple(ct[i+j] for j in range(16) if i+j < len(ct)))
+
+        unique_chunks = len(set(chunks))
+        repetitions = len(chunks) - unique_chunks # going to look for line with most repeating blocks of 16 bytes
+        print(len(chunks), unique_chunks, repetitions)
+        if repetitions > 0:
+            detected_mode = 'ECB'
+        else:
+            detected_mode = 'CBC'
+        print('probably', detected_mode)
+        assert real_mode == detected_mode
