967d0ed37a392b113a12d1a48306c8438b838a3a
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2021-05-28 09:59:18 +0100

    wip

--- set2/challenge11.py
+++ set2/challenge11.py
@@ -28,10 +28,20 @@ def encryption_oracle(input):
     input = prefix + input + suffix
 
     if random.randint(0, 1) == 1: # assuming this is equivalent to rand(2)
+        print('Mode: ECB')
         return encrypt_aes_ecb(pad(input, 16), key)
     else:
+        print('Mode: CBC')
         return encrypt_aes_cbc(input, key, os.urandom(16))
 
 if __name__ == "__main__":
-    print(list(encryption_oracle(b"YELLOW SUBMARINE")))
+    ct = list(encryption_oracle(b"ABCDABCDABCDABCD"))
+    print(ct)
     # TODO: Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
+
+    chunks = []
+    for i in range(0, len(ct), 16):
+        chunks.append(tuple(ct[i+j] for j in range(16) if i+j < len(ct)))
+    unique_chunks = len(set(chunks))
+    repetitions = len(chunks) - unique_chunks # going to look for line with most repeating blocks of 16 bytes
+    print(len(chunks), unique_chunks, repetitions)
