faa3e52b2270ac09ef6d7db31174639fec4cf5a1
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2021-06-02 03:28:11 +0100

    challenge9: oops

--- set2/challenge9.py
+++ set2/challenge9.py
@@ -1,6 +1,6 @@
 def pad(pt, block_length):
     padding_length = block_length - (len(pt) % block_length)
-    return pt + (padding_length * b'\x04')
+    return pt + (padding_length * chr(padding_length).encode('ascii'))
 
 if __name__ == "__main__":
     input = b"YELLOW SUBMARINE", 20
